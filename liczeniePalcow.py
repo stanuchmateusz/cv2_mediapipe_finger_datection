2b82303b5693a0bd800034198ac72646c7b5ee3b
MateuszStanuch <matitonie@o2.pl>
2022-01-18 13:21:33 +0100

    naprawa orientacji dłoni

--- liczeniePalcow.py
+++ liczeniePalcow.py
@@ -10,11 +10,11 @@ class liczeniePalcow:
             (5, "mały"): False,
         }
         self.id_punktow = {
-            'koncowych': [4, 8, 12, 16, 20],
+            'koncowych': [3, 8, 12, 16, 20],
             'poczatkowych': [5, 6, 10, 14, 17]
         }
         self.__liczeniePalcow()
-        self.orientacja = self.__orientacjaPalcow()
+        self.orientacja = self.__orientacjaDloni()
         self.__sprawdzPalce()
 
     def __liczeniePalcow(self):
@@ -25,26 +25,33 @@ class liczeniePalcow:
             self.lista_lm.append([id, cx, cy])
         return self.lista_lm
 
-    def __orientacjaPalcow(self):
+    def __orientacjaDloni(self):
         # sprawdź czy to prawa albo lewa ręka
-        # jesli x punktu 0 jest mniejsze od x punktu 1
-        if self.lista_lm[1][1] < self.lista_lm[0][1]:
+        # jesli x punktu 0 jest większe od x punktu 1
+        if self.lista_lm[0][1] > self.lista_lm[1][1]:
             return 'lewa'
         else:
             return 'prawa'
 
     def __sprawdzPalce(self):
         for id, nazwa in self.palce:
+            k = self.id_punktow['koncowych'][id-1]
+            p = self.id_punktow['poczatkowych'][id-1]
             # dla kciuka  lm[id,x,y]
-            if id == 1 and self.lista_lm[self.id_punktow['koncowych'][0]][2] > self.lista_lm[self.id_punktow['poczatkowych'][0]][2]:
-                self.palce[(id, nazwa)] = True
+            if id == 1:
+                if self.orientacja == 'lewa':
+                    if self.lista_lm[k][1] < self.lista_lm[p][1]:
+                        self.palce[id, nazwa] = True
+                else:
+                    if self.lista_lm[k][1] > self.lista_lm[p][1]:
+                        self.palce[id, nazwa] = True
             else:
-                if self.lista_lm[self.id_punktow['koncowych'][id-1]][2] < self.lista_lm[self.id_punktow['poczatkowych'][id-1]][2]:
+                if self.lista_lm[k][2] < self.lista_lm[p][2]:
                     self.palce[(id, nazwa)] = True
 
     def wypisz_status(self):
         print(10*'-')
-        print(f'Orientacja palcow: {self.orientacja}')
+        print(f'Orientacja dloni: {self.orientacja}')
         for i, p in self.palce:
             print(f'{p} {self.palce[(i,p)]}')
         print(10*'-')
